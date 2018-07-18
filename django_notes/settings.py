0f8db42683b147dcbf4bf4530dbadfea7a85a07d
shandour <shandour@lol>
2018-07-18 16:51:36 +0300

    more heroku-related tweaks

--- django_notes/settings.py
+++ django_notes/settings.py
@@ -72,10 +72,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
 # apply heroku settings
 import django_heroku
 django_heroku.settings(locals())
